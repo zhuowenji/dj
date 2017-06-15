f71152ff873cd0c9f69e5e0e7129a35f71bed89c
zhuowenji <44208300@qq.com>
2017-06-15 11:12:11 +0800

    *

--- function.js
+++ function.js
@@ -1,3 +1,13 @@
 function ClearTextArea(){
     document.getElementById("textarea").value="";
 }
+
+
+//百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?36fd4438cc53b54f7c07ed5858f67adf";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
