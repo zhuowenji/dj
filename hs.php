4af3c1893d11b155cdaf7ac13cc94e1231aa8177
zhuowenji <44208300@qq.com>
2017-06-15 10:39:03 +0800

    *

--- hs.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-for ($i = 0; $i < 10; $i++) {
-    echo '<script>window.open("http://hotsoon.snssdk.com/hotsoon/item/video/_playback/?video_id=4f6a18f256c04c4999bcc4fd4e2c54ee&line=0&watermark=1");</script>';
-}
-
-echo 'text';
