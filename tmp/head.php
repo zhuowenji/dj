ccebe2c2077ff688a0a4c95031374a5fa8c3d2a0
zhuowenji <zhuowenji@qq.com>
2017-06-18 15:43:49 +0800

    *

--- tmp/head.php
+++ tmp/head.php
@@ -2,15 +2,13 @@
 <html lang="zh-CN">
   <head>
     <meta charset="UTF-8" />
-    <title>头尾神策</title>
-    <script type="text/javascript" src="function.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="keywords" content="七星彩,头尾,七星彩头尾,七星彩论坛,湛江私彩,头尾规律">
     <meta name="description" content="海南湛江七星彩，头尾，头尾神策，60组神码">
-<title>
-    头尾神策 fuxiben.com
-</title>
-
+    <title>
+        头尾神策 fuxiben.com
+    </title>
+    <script type="text/javascript" src="style/js/function.js"></script>
     <link href="style/css/bootstrap.css" rel="stylesheet">
     <link href="style/css/docs.css" rel="stylesheet">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -25,9 +23,9 @@
   </head>
   <body>
         <!-- Docs master nav -->
-    <header class="navbar navbar-static-top bs-docs-nav" id="top">
-        <div class="container">
-            <div class="navbar-header">
+    <header class="navbar navbar-default bs-docs-nav" id="top">
+        <div class="container navbar-fixed-top">
+            <div class="navbar-header ">
               <a href="../" class="navbar-brand">首页</a>
               <a href="wx.php" class="navbar-brand">微信群</a>
             </div>
