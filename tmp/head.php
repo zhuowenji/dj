ae51c23c52cc73df2bfc519235a89283cad88e10
zhuowenji <zhuowenji@qq.com>
2017-06-18 20:53:45 +0800

    *

--- tmp/head.php
+++ tmp/head.php
@@ -29,7 +29,7 @@
               <a href="/" class="navbar-brand">首页</a>
               <a href="60.php" class="active navbar-brand">60组码</a>
               <a href="search.php" class="active navbar-brand">查码</a>
-              <a href="liangen.php" class="navbar-brand">连跟码</a>
+              <!-- <a href="liangen.php" class="navbar-brand">连跟码</a> -->
               <a href="wx.php" class="navbar-brand">微群</a>
             </div>
         </div>
