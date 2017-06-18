43c1d589a768138ba3d0fa0d8b70fe52a87984a5
zhuowenji <zhuowenji@qq.com>
2017-06-18 18:16:41 +0800

    *

--- tmp/index_content.php
+++ tmp/index_content.php
@@ -36,8 +36,8 @@
     <h3><?php echo $year ?>年开奖记录</h3>
     <table class="table table-bordered">
         <tr>
-            <th>时间</th>
             <th>期数</th>
+            <th>时间</th>
             <th>头-尾</th>
             <th>大奖</th>
         </tr>
