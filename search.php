b2f61d0b0acb5a3b8c980340c6aab82aded6f2dd
zhuowenji <zhuowenji@qq.com>
2017-06-18 18:03:03 +0800

    *

---
+++ search.php
@@ -0,0 +1,114 @@
+<?php
+include 'config.php';
+include 'function.php';
+
+//获取所有数据
+$mysqli = connect();
+$kj = getAll($mysqli);
+
+foreach ($kj as $key => $value) {
+    $y         = date('Y', strtotime($value['time']));
+    $all[$y][] = $value;
+}
+
+//搜索处理
+$da = ['3-5', '6-7'];
+if (isset($_GET['number']) && !empty($_GET['number'])) {
+    $number = trimall($_GET['number']);
+    $da     = array_unique(array_filter(explode(',', $number)));
+    $re     = [];
+    foreach ($da as $value) {
+        $tou_wei = explode('-', trim($value));
+        if (count($tou_wei) == 1) {
+            continue;
+        }
+
+        $tou = $tou_wei[0];
+        $wei = $tou_wei[1];
+
+        for ($i = 0; $i < strlen($tou); $i++) {
+            $str_tou = substr($tou, $i, 1);
+            if (is_numeric($str_tou)) {
+                for ($w = 0; $w < strlen($wei); $w++) {
+                    $str_wei = substr($wei, $w, 1);
+                    if (is_numeric($str_wei)) {
+                        $re[] = $str_tou . '-' . $str_wei;
+                    }
+                }
+            }
+        }
+    }
+    $da = array_unique($re);
+
+    //纪录查询记录
+    $ip     = $_SERVER['REMOTE_ADDR'];
+    $da_log = json_encode($da);
+    $sql    = "INSERT INTO `search_log` (`id`, `content`, `ip`, `time`) VALUES (NULL, ' " . $da_log . "', '" . $ip . "', CURRENT_TIMESTAMP);";
+    $res    = $mysqli->query($sql);
+    if ($res === false) {
+        var_dump($mysqli->errno);
+        var_dump($mysqli->error);
+    }
+}
+
+//稳赚算法
+$mqz = 500;
+if (isset($_GET['mqz']) && $_GET['mqz']) {
+    $mqz = $_GET['mqz'];
+}
+
+$qs = 5;
+if (isset($_GET['qs']) && $_GET['qs']) {
+    $qs = $_GET['qs'];
+}
+
+$wenzhuan = Wenzhuantongji($da, $mqz, $qs);
+
+$data = [];
+foreach ($all as $nian => $list) {
+
+    $ljlxbz  = 1;
+    $lxbz    = 0;
+    $buzhong = 0;
+    $zhong   = [];
+    foreach ($list as $key => $value) {
+        $tou     = substr($value['number'], 0, 1);
+        $wei     = substr($value['number'], 3, 1);
+        $dajiang = substr($value['number'], 0, 4);
+
+        $touwei = $tou . '-' . $wei;
+
+        if (!in_array($touwei, $da)) {
+            //计算最长连续不中
+            $lxbz = $lxbz + 1;
+            if ($lxbz > $buzhong) {
+                //累计连续不中最大次数重置
+                $ljlxbz  = 1;
+                $buzhong = $lxbz;
+                $bzsj    = $value['time'];
+            } elseif ($lxbz == $buzhong) {
+                $ljlxbz++;
+            }
+        } else {
+            //已中奖，重置连续中奖值
+            $lxbz                  = 0;
+            $zhong[$value['time']] = ['period' => $value['period'], 'number' => $dajiang];
+        }
+    }
+
+    $data[$nian]['year']    = $nian;
+    $data[$nian]['buzhong'] = $buzhong;
+    $data[$nian]['bzsj']    = $bzsj;
+    $data[$nian]['ljlxbz']  = $ljlxbz;
+    $data[$nian]['kai']     = count($list);
+    $data[$nian]['zhong']   = count($zhong);
+    $data[$nian]['gailv']   = count($zhong) / count($list);
+}
+
+
+include 'tmp/head.php';
+include 'tmp/search_content.php';
+include 'tmp/foot.php';
+
+
+
---
+++ tmp/search_content.php
@@ -0,0 +1,75 @@
+<div class="bs-docs-header" id="content" tabindex="-1">
+  <div class="container">
+    <h1>输入头尾号码<small></small></h1>
+    <p>3头5,6头7 单组模式：3-5,6-7,头尾模式：36-57</p>
+  </div>
+</div>
+
+<div class="container">
+    <form method="get" action="search.php">
+        <div class="form-group">
+            <textarea id="textarea" class="form-control" rows="3" name="number" placeholder="3-5,6-7,"><?php echo isset($_GET['number']) && !empty($_GET['number']) ? $_GET['number'] : ''; ?></textarea>
+        </div>
+        <div class="form-group">
+            <label>每期赚多少</label>
+            <input type="text" class="form-control" id="mqz" name="mqz" placeholder="500" value="<?php echo isset($_GET['mqz']) && !empty($_GET['mqz']) ? $_GET['mqz'] : ''; ?>">
+        </div>
+        <div class="form-group">
+            <label>连打多少期</label>
+            <input type="text" class="form-control" id="qs" name="qs" placeholder="5" value="<?php echo isset($_GET['qs']) && !empty($_GET['qs']) ? $_GET['qs'] : ''; ?>">
+        </div>
+        <button type="submit" class="btn btn-default">查询</button>
+        <button type="button" onclick="ClearTextArea()" class="btn btn-default">清除</button>
+    </form>
+
+    <h3>查询结果 <small><?php echo count($da); ?> 组头尾</small></h3>
+
+    <?php if (count($da) > 0) {?>
+    <blockquote><p><?php echo implode(',', $da); ?></p></blockquote>
+    <?php } else {?>
+    <blockquote><font color="red">查询输入有误,请重新输入</font></blockquote>
+    <?php }?>
+
+    <h4><small>连跟号码每期稳赚统计</small></h4>
+    <table class="table table-bordered">
+        <tr>
+            <th>连跟期数(期)</th>
+            <th>每码需打(组)</th>
+            <th>中了得到</th>
+            <th>需投资(本)</th>
+            <th>赚</th>
+        </tr>
+        <?php foreach ($wenzhuan as $info) {?>
+        <tr>
+            <td><?php echo $info['qishu']; ?></td>
+            <td><?php echo $info['djzs']; ?></td>
+            <td><?php echo $info['sunshi']; ?></td>
+            <td><font color="red"><?php echo $info['ben']; ?></font></td>
+            <td><?php echo $info['zhuan']; ?></td>
+        </tr>
+        <?php }?>
+    </table>
+
+    <table class="table table-bordered">
+        <tr>
+            <th>年份时间</th>
+            <th>开奖次数</th>
+            <th>中奖次数</th>
+            <th>中奖概率</th>
+            <th>最大连续不中次数</th>
+        </tr>
+        <?php foreach ($data as $info) {?>
+        <tr>
+            <td><?php echo $info['year']; ?></td>
+            <td><?php echo $info['kai']; ?></td>
+            <td><?php echo $info['zhong']; ?></td>
+            <td><?php echo sprintf('%.2f', $info['gailv'] * 100); ?>%</td>
+            <td>
+                <?php echo $info['buzhong']; ?><br/>
+                时间： <?php echo $info['bzsj']; ?><br/>
+                <?php echo $info['ljlxbz'] > 1 ? "连续 {$info['buzhong']} 期不中出现过 {$info['ljlxbz']} 次数" : ''; ?>
+            </td>
+        </tr>
+        <?php }?>
+    </table>
+</div>
