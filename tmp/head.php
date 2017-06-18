03ac8bc39bdb04390ae96eb64d94cf7321e8e60f
zhuowenji <zhuowenji@qq.com>
2017-06-18 15:23:03 +0800

    *

--- tmp/head.php
+++ tmp/head.php
@@ -29,16 +29,8 @@
     <!-- Docs master nav -->
     <header class="navbar navbar-static-top bs-docs-nav" id="top">
       <div class="container">
-        <div class="navbar-header">
-          <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-navbar" aria-controls="bs-navbar" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        </div>
-        <nav id="bs-navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
+        <nav>
+            <ul class="nav nav-tabs">
                 <li>
                   <a href="/">头尾神策首页</a>
                 </li>
