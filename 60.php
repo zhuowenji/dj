4e0f603fb6928a2ccfefde2bc6249d4f1a90eb3a
zhuowenji <zhuowenji@qq.com>
2017-06-18 16:30:28 +0800

    *

---
+++ 60.php
@@ -0,0 +1,32 @@
+<?php
+
+include 'config.php';
+include 'function.php';
+
+//连接获取数据
+$mysqli = connect();
+$sql    = 'select * from kaijiang order by id desc';
+$res    = $mysqli->query($sql);
+if ($res === false) {
+    var_dump($mysqli->errno);
+    var_dump($mysqli->error);
+}
+
+$kj = [];
+while ($data = $res->fetch_array()) {
+    $kj[] = $data;
+}
+
+//推荐号码
+$tuijian = tuijian($kj);
+
+$ex_tuijian = explode(',', $tuijian);
+$sixty = [];
+foreach (array_filter($ex_tuijian) as $info) {
+	$num = explode('-', $info);
+	$sixty[$num[0]] = $num[1]; 
+}
+
+include 'tmp/head.php';
+include 'tmp/60_content.php';
+include 'tmp/foot.php';
---
+++ tmp/60_content.php
@@ -0,0 +1,30 @@
+<div class="bs-docs-header" id="content" tabindex="-1">
+  <div class="container">
+    <h1>推荐60组头尾</h1>
+    <p>
+        <small>最近连续中奖9次</small>
+    </p>
+  </div>
+</div>
+<div class="container">
+    <h3>
+        <small><a href="index.php?number=<?php echo $tuijian; ?>">点此查看概率</a></small>
+    </h3>
+
+	<div class="table-responsive">
+		<table class="table">
+			<tr>
+				<th>头</th>
+				<th>尾</th>
+				<th>组数</th>
+			</tr>
+			<?php foreach ($sixty as $key => $info) { ?>
+			<tr>
+				<td><?php echo $key ?></td>
+				<td><?php echo $info ?></td>
+				<td><?php echo strlen($info) ?></td>
+			</tr>
+			<?php } ?>
+		</table>
+	</div>
+</div>
