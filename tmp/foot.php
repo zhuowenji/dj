06dea2a0b6a78bd1eb42295b28e2b4d83b2162fc
zhuowenji <44208300@qq.com>
2017-06-23 15:01:57 +0800

    *

--- tmp/foot.php
+++ tmp/foot.php
@@ -2,9 +2,10 @@
 	  <div class="container">
 	    <ul class="bs-docs-footer-links">
 	      <li><a href="wx.php">微信群，加入我们</a></li>
+          <li>安全运营 <?php echo runtime(); ?> 天</li>
 	    </ul>
 	    <p>欢迎访问头尾神策、无广告、永久免费 www.fuxiben.com</p>
-        <p>安全运营 <?php echo runtime(); ?> 天</p>
+        <p></p>
 	  </div>
 	</footer>
 
