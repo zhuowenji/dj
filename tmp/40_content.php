86b48c8b9b40395d0ba0549827eac9e05ed6020d
zhuowenji <zhuowenji@qq.com>
2017-06-25 13:59:12 +0800

    *

--- tmp/40_content.php
+++ tmp/40_content.php
@@ -59,7 +59,7 @@
                 <td><?php echo $kai['time'] ?: '<span class="glyphicon glyphicon-time" aria-hidden="true"></span>'; ?></td>
                 <td><?php echo $kai['number'] ?: '<span class="glyphicon glyphicon-time" aria-hidden="true"></span>'; ?></td>
                 <td>
-                    <a href="60.php?tj=<?php echo $info['period']; ?>" ?>
+                    <a href="40.php?tj=<?php echo $info['period']; ?>" ?>
                     <?php echo $info['count']; ?>
                     </a>
                 </td>
