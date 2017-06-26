a4a457e6db0fb9b8c37039008fd05b073daff470
zhuowenji <44208300@qq.com>
2017-06-26 17:12:13 +0800

    *

--- tmp/admin_login.php
+++ tmp/admin_login.php
@@ -8,15 +8,15 @@
 <div class="container">
     <form method="post">
       <div class="form-group">
-        <label for="username">用户名</label>
+        <label for="username"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> 用户名</label>
         <input type="text" class="form-control" name="username" id="username" placeholder="Username" required="required" >
       </div>
       <div class="form-group">
-        <label for="password">密码</label>
+        <label for="password"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> 密&nbsp;&nbsp;&nbsp;码</label>
         <input type="password" class="form-control" name="password" id="password" placeholder="Password" required="required">
       </div>
       <div class="form-group">
-        <label for="exampleInputFile"><font color="red"><?php echo $msg ?: ''; ?></font></label>
+        <label for="exampleInputFile"><font color="red"><?php echo $msg ?: '不要随意尝试，后果很严重'; ?></font></label>
       </div>
       <button type="submit" class="btn btn-default">登 录</button>
     </form>
