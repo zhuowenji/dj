cb1d0a70f51bb31c97bb70c3068d82075f6a4e7c
zhuowenji <44208300@qq.com>
2017-07-04 23:00:07 +0800

    *

--- test/zj.php
+++ test/zj.php
@@ -1,5 +1,7 @@
 <?php
 
+include '../function.php';
+
 $arr = [
     '6月' => [
         '2017-6-18' => '-300',
@@ -14,53 +16,62 @@ $arr = [
     ],
 ];
 
+include '../tmp/head.php';
+
+?>
+<div class="bs-docs-header" id="content" tabindex="-1">
+  <div class="container">
+        <h1>
+        个人战绩
+        <p><small>不怕输，只怕奖无开</small></p>
+        </h1>
+  </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <link href="../style/css/bootstrap.css" rel="stylesheet">
+        <?php $zong = 0;?>
+        <?php foreach ($arr as $time => $info) {?>
+            <?php $count = array_sum($info);?>
+            <div class="col-md-3">
+                <table class="table table-bordered">
+                    <tr>
+                        <th>场次</th>
+                        <th>时间</th>
+                        <th>成绩</th>
+                    </tr>
+
+                    <?php $a     = 0;?>
+                    <?php $sheng = 0;?>
+
+                    <?php foreach ($info as $key => $value) {?>
+                    <?php $a++;?>
+                    <tr>
+                        <td><?php echo $a; ?></td>
+                        <td><?php echo date('Y年m月d日', strtotime($key)); ?></td>
+                        <td><?php echo $value; ?></td>
+                    </tr>
+                    <?php $value > 0 ? $sheng++ : '';?>
+                    <?php }?>
+                    <tr>
+                        <td><?php echo $time; ?>总</td>
+                        <td colspan="2"><font color="red"><b><?php echo $count . ' - ' . number_format($sheng / $a * 100, 2, '.', '') . '%'; ?></b></font></td>
+                    </tr>
+                </table>
+            </div>
+            <?php $zong += $count;?>
+        <?php }?>
+    </div>
+
+    <div class="col-md-12">
+            <h3><pre>总战绩：<?php echo $zong; ?></pre></h3>
+    </div>
+</div>
+
+<?php
+
+include '../tmp/foot.php';
+
 ?>
 
-<style type="text/css">
-
-tr { padding:20px;}
-
-</style>
-
-<html>
-    <div class="container">
-        <div class="row">
-            <link href="../style/css/bootstrap.css" rel="stylesheet">
-            <?php $zong = 0;?>
-            <?php foreach ($arr as $time => $info) {?>
-                <?php $count = array_sum($info);?>
-                <div class="col-md-3">
-                    <table class="table table-bordered">
-                        <tr>
-                            <th>场次</th>
-                            <th>时间</th>
-                            <th>成绩</th>
-                        </tr>
-
-                        <?php $a     = 0;?>
-                        <?php $sheng = 0;?>
-
-                        <?php foreach ($info as $key => $value) {?>
-                        <?php $a++;?>
-                        <tr>
-                            <td><?php echo $a; ?></td>
-                            <td><?php echo date('Y年m月d日', strtotime($key)); ?></td>
-                            <td><?php echo $value; ?></td>
-                        </tr>
-                        <?php $value > 0 ? $sheng++ : '';?>
-                        <?php }?>
-                        <tr>
-                            <td><?php echo $time; ?>总</td>
-                            <td colspan="2"><font color="red"><b><?php echo $count . ' - ' . number_format($sheng / $a * 100, 2, '.', '') . '%'; ?></b></font></td>
-                        </tr>
-                    </table>
-                </div>
-                <?php $zong += $count;?>
-            <?php }?>
-        </div>
-
-        <div class="col-md-12">
-                <h3><pre>总战绩：<?php echo $zong; ?></pre></h3>
-        </div>
-    </div>
-</html>
