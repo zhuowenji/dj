5dbcf50257bb29c276644c30e3094f516d2858dd
zhuowenji <44208300@qq.com>
2017-08-02 16:08:46 +0800

    *

--- tmp/head.php
+++ tmp/head.php
@@ -41,9 +41,9 @@
             </div>
             <nav id="bs-navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
               <ul class="nav navbar-nav">
-                <li><a href="/search.php">查码</a></li>
-                <li><a href="/40.php" >40组码</a></li>
-                <li><a href="/liangen.php" >连跟码</a></li>
+                <li><a href="/40.php" >40专区</a></li>
+                <li><a href="/search.php">号码概率查询</a></li>
+                <li><a href="/liangen.php" >连跟码计算</a></li>
                 <li><a href="/jisuan.php" >收益计算</a></li>
               </ul>
             </nav>
