73c7040bab9ad9510bdfbe7a5b3d9f1a2144495c
zhuowenji <zhuowenji@qq.com>
2017-08-09 03:45:23 +0800

    *

--- tmp/index_content.php
+++ tmp/index_content.php
@@ -52,32 +52,4 @@
             </tr>
         <?php }?>
     </table>
-
-        <h3> 近5年头尾各重复出现次数 </h3>
-    <table class="table table-bordered">
-        <tr>
-            <th></th>
-            <th>年份／头次数</th>
-            <th>年份／尾次数</th>
-        </tr>
-        <?php for ($i = 0; $i <= 9; $i++) {?>
-        <tr>
-            <td><?php echo $i; ?></td>
-            <td>
-                <?php for ($start_year = 2013; $start_year <= $year; $start_year++) {?>
-                    <?php $tou = $year_tou[$start_year][$i];?>
-                    <?php echo $start_year . '／' . $tou; ?>
-                    <br/>
-                <?php }?>
-            </td>
-            <td>
-                <?php for ($start_year = 2013; $start_year <= $year; $start_year++) {?>
-                    <?php $wei = $year_wei[$start_year][$i];?>
-                    <?php echo $start_year . '／' . $wei; ?>
-                    <br/>
-                <?php }?>
-            </td>
-        </tr>
-        <?php }?>
-    </table>
 </div>
