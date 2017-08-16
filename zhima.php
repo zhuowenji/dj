c84552c085667dee28f787a1dda06de094818381
zhuowenji <44208300@qq.com>
2017-08-16 17:17:53 +0800

    *

--- zhima.php
+++ zhima.php
@@ -69,6 +69,8 @@ $zhima['ge']   = array_count_values($ge);
 
 $dama = zhima($zhima, $sha_liu);
 
+var_dump($dama);
+
 function zhima($zhima, $sha_liu, $number = 4)
 {
 
@@ -86,11 +88,11 @@ function zhima($zhima, $sha_liu, $number = 4)
     }
 
     if (isset($sha_liu['sha_shi'])) {
-        unset($zhima['shi'][$sha_liu['sha_qian']]);
+        unset($zhima['shi'][$sha_liu['sha_shi']]);
     }
 
     if (isset($sha_liu['sha_ge'])) {
-        unset($zhima['ge'][$sha_liu['sha_qian']]);
+        unset($zhima['ge'][$sha_liu['sha_ge']]);
     }
 
     $qian = array_slice(array_keys($zhima['qian']), 0, $number);
