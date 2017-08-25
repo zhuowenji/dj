35741e30234a6fd4b683a4b9bfcd9db49e9119d8
zhuowenji <44208300@qq.com>
2017-08-25 21:17:11 +0800

    *

--- houtai/task/50_task.php
+++ houtai/task/50_task.php
@@ -60,27 +60,27 @@ if ($mysqli->affected_rows) {
             $msg .= '50码更新失败、';
             die;
         }
-        echo $up_sql;
+
         $msg .= '50码更新成功、';
     }
 }
 
 //推荐号码
-$qian       = array_splice($kj, 0, 3);
-$tuijian    = SixtyTuijian($kj, $qian);
-$new_period = $kj_id + 1;
+$qian              = array_splice($kj, 0, 3);
+$tuijian           = SixtyTuijian($kj, $qian);
+$new_period        = $kj_id + 1;
 $new_period_actual = $kj_period + 1;
 
 //下一期开码时间
 $new_time = strtotime($kj_time);
-$num =  date("N",$new_time);
-if($num == '7' || $num == '5'){
+$num      = date('N', $new_time);
+if ($num == '7' || $num == '5') {
     $new_time += 172800;
-}elseif($num == '2'){
+} elseif ($num == '2') {
     $new_time += 259200;
 }
 
-$tj_install = 'INSERT INTO `tj` (`number`, `type`, `count`, `period`,`period_actual`, `open_time`, `win`, `create_time`) VALUES ("' . $tuijian['number'] . '", ' . '50,' . $tuijian['number_count'] . ', ' . $new_period .','.$new_period_actual.','.$new_time.', NULL, ' . time() . ');';
+$tj_install = 'INSERT INTO `tj` (`number`, `type`, `count`, `period`,`period_actual`, `open_time`, `win`, `create_time`) VALUES ("' . $tuijian['number'] . '", ' . '50,' . $tuijian['number_count'] . ', ' . $new_period . ',' . $new_period_actual . ',' . $new_time . ', NULL, ' . time() . ');';
 $install    = $mysqli->query($tj_install);
 if ($install === false) {
     var_dump($mysqli->errno);
