b86c1934f664543f61827738279bfafdca2a7b37
zhuowenji <44208300@qq.com>
2017-09-24 21:19:38 +0800

    *

---
+++ test/chaifen.php
@@ -0,0 +1,26 @@
+
+<form>
+
+<input type="test" name="ma" value="<?php echo isset($_GET['ma']) ? $_GET['ma'] : ''; ?> ">
+<button>提交</button>
+</form>
+<?php
+
+$chai = '';
+
+if (isset($_GET['ma'])) {
+    $chai = $_GET['ma'];
+}
+
+$ma = str_split($chai, 1);
+
+$c = '';
+foreach ($ma as $v) {
+    foreach ($ma as $s) {
+        if ($s != $v) {
+            $c .= $v . $s . ',';
+        }
+    }
+}
+
+echo substr($c, 0, -1);
