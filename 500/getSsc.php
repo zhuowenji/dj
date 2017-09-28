6c06c56d82829d2c8650bb8d98daf656eaf6a662
zhuowenji <44208300@qq.com>
2017-09-29 06:17:54 +0800

    *

--- 500/getSsc.php
+++ 500/getSsc.php
@@ -17,8 +17,7 @@ for ($i = 1; $i < 1002; $i++) {
 $preg = '/var Con_BonusCode ' . $dian . '/i';
 preg_match($preg, $content, $data);
 
-$new = str_replace(['"', ';'], ['', ''], substr($data[0], -20));
-
+$new  = str_replace(['"', ';'], ['', ''], substr($data[0], -20));
 $chai = explode('=', $new);
 
 //数据库操作
@@ -63,11 +62,48 @@ $xia_row = mysqli_fetch_array($xia_res, MYSQLI_ASSOC);
 if ($xia_row != null) {
     exit;
 }
-//这时候要加入号码
-//$tuijian = '01,02,03,04,05,06,08,09,10,11,12,14,16,17,19,20,21,22,23,24,25,29,30,31,32,33,35,36,37,38,40,41,42,43,44,47,48,50,51,53,56,58,59,60,61,62,63,64,68,69,70,71,72,73,74,75,76,79,80,81,82,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99';
 
-//$sql = "INSERT INTO `ssc` (`id`, `number`, `periods`, `tuijian`, `status`, `time`) VALUES (NULL, NULL,'" . $chai[0] . "', '" . $tuijian . "', '0', '" . $time . "');";
+//获取推荐码
+$tuijian_sql = 'select number from ssc where number is not null  order by id desc limit 20';
+$tuijian_res = $mysqli->query($tuijian_sql);
 
-$sql = "INSERT INTO `ssc` (`id`, `number`, `periods`, `tuijian`, `status`, `time`) VALUES (NULL, NULL,'" . $chai[0] . "', NULL, '0', '" . $time . "');";
+$tuijian_unmber = [];
+while ($info = $tuijian_res->fetch_array()) {
+    $tuijian_unmber[] = $info;
+}
 
+$number = '';
+foreach ($tuijian_unmber as $info) {
+    $number .= substr($info['number'], 0, 3) . ',';
+}
+
+//去除最近出现过的一个值
+$sub_quchu = substr($number, 0, 3);
+$rep_quchu = str_replace(',', '', $sub_quchu);
+
+$suoyou = explode(',', $number);
+
+$tiqu = [];
+foreach ($suoyou as $v) {
+    if (in_array($v, $tiqu) || count($tiqu) == 8) {
+        continue;
+    }
+
+    $tiqu[] = $v;
+}
+sort($tiqu);
+
+$tuijian = '';
+foreach ($tiqu as $v) {
+    foreach ($tiqu as $s) {
+        if ($s != $v) {
+            $xuyao = $v . $s;
+            if ($xuyao != $rep_quchu) {
+                $tuijian .= $xuyao . ',';
+            }
+        }
+    }
+}
+
+$sql = "INSERT INTO `ssc` (`id`, `number`, `periods`, `tuijian`, `status`, `time`) VALUES (NULL, NULL,'" . $chai[0] . "', '" . $tuijian . "', '0', '" . $time . "');";
 $mysqli->query($sql);
